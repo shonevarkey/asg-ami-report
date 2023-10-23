8c260ddc278b076646e994a3f8d56fdc1c5ce239
shonevarkey <shonevarkey2012@gmail.com>
2023-10-23 19:14:36 +0530

    Update aws_report_script.py

--- aws_report_script.py
+++ aws_report_script.py
@@ -6,7 +6,8 @@ import csv
 
 def get_account_id():
     sts_client = boto3.client('sts')
-    account_id = sts_client.get_caller_identity()['Account']
+    account_id = sts_client.get_caller_identity()
+    return response['Account']
 
 def get_instance_details(instance_id):
     
